bccaa6f6c93f295697663cc65eb3cd1e01fa48d3
yuheiyamakawa <ymkwyh1@gmail.com>
2025-09-28 05:57:06 +0900

    Update portfolio with Tailwind config and CSS fixes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind が効くファイルを指定
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
